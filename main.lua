399ea4b59439f4124037e8f47d0bc1254f4a6eee
Pirnogion <glebprositapow@gmail.com>
2019-11-29 17:29:35 +0500

    Unified minimaps

--- main.lua
+++ main.lua
@@ -30,10 +30,11 @@ local gpu = love.graphics
 local screenWidth, screenHeight = gpu.getWidth(), gpu.getHeight()
 
 local juliaSet, juliaSetIsDrawed = nil, false
+local mandelbrotSet, mandelbrotSetIsDrawed = nil, false
 local minimap, minimapIsDrawed = nil, false
-local shader = nil
+local mainShader, mandelbrotShader = nil, nil
 
-local const = {-0.0085, 0.71} --{-0.6655218, 0.234455}
+local const = {0.0, 0.0}
 local r = (1+math.sqrt(1+4*(const[1]*const[1]+const[2]*const[2])))/2 + 0.6
 
 local isChangingIterations = false
@@ -145,14 +146,40 @@ function love.load()
         }
     ]]
 
+    local mandelbrot = [[
+		extern float iterations;
+
+		float map(float x, float in_min, float in_max, float out_min, float out_max)
+		{
+			return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+		}
+
+        vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 pixel_coords)
+        {
+        	vec2 point = vec2(map(pixel_coords.x, love_ScreenSize.x-60*2 - 5, love_ScreenSize.x - 5, -2, 2), map(pixel_coords.y, 5, 60*2 + 5, -2, 2));
+
+        	vec2 temp = vec2(0, 0);
+        	float j;
+			for (j = 0; j < iterations && dot(temp, temp) < 4; ++j)
+				temp = vec2(temp.x*temp.x - temp.y*temp.y, 2*temp.x*temp.y) + point;
+
+            return vec4(0.0, 0.1373, 0.4, j / iterations);
+        }
+    ]]
+
     juliaSet = gpu.newCanvas()
+    mandelbrotSet = gpu.newCanvas()
     minimap = gpu.newCanvas()
-    shader = gpu.newShader(julia)
+
+    mainShader = gpu.newShader(julia)
+    mandelbrotShader = gpu.newShader(mandelbrot)
 end
 
 function love.update(dt)
+	r = (1+math.sqrt(1+4*(const[1]*const[1]+const[2]*const[2])))/2 + 0.6
+
 	selector:update(dt, mouse)
-	const[1], const[2] = selector.value.x, -selector.value.y
+	const[1], const[2] = selector.value.x * 2, -selector.value.y * 2
 
 	if mouse.isDragging and not selector.isSelecting then
 		local x, y = love.mouse.getX(), love.mouse.getY()
@@ -176,12 +203,12 @@ function love.draw()
 	if not juliaSetIsDrawed then
 		gpu.setCanvas(juliaSet)
 			gpu.clear()
-			gpu.setShader(shader)
-				shader:send("iterations", iterations)
-				shader:send("radius",     r)
-				shader:send("zoom",       zoom)
-				shader:send("shift",      shift)
-				shader:send("constant",   const)
+			gpu.setShader(mainShader)
+				mainShader:send("iterations", iterations)
+				mainShader:send("radius",     r)
+				mainShader:send("zoom",       zoom)
+				mainShader:send("shift",      shift)
+				mainShader:send("constant",   const)
 			    gpu.rectangle('fill', 0, 0, gpu.getWidth(), gpu.getHeight())
 			gpu.setShader()
 		gpu.setCanvas()
@@ -189,15 +216,27 @@ function love.draw()
 		juliaSetIsDrawed = true
 	end
 
+	if not mandelbrotSetIsDrawed then
+		gpu.setCanvas(mandelbrotSet)
+			gpu.clear()
+			gpu.setShader(mandelbrotShader)
+				mandelbrotShader:send("iterations", 32)
+			    gpu.rectangle('fill', 0, 0, gpu.getWidth(), gpu.getHeight())
+			gpu.setShader()
+		gpu.setCanvas()
+
+		mandelbrotSetIsDrawed = true
+	end
+
 	if not minimapIsDrawed then
 		gpu.setCanvas(minimap)
 			gpu.clear()
-			gpu.setShader(shader)
-				shader:send("iterations", 32)
-				shader:send("radius",     r)
-				shader:send("zoom",       1)
-				shader:send("shift",      {0, 0})
-				shader:send("constant",   const)
+			gpu.setShader(mainShader)
+				mainShader:send("iterations", 32)
+				mainShader:send("radius",     r)
+				mainShader:send("zoom",       1)
+				mainShader:send("shift",      {0, 0})
+				mainShader:send("constant",   const)
 			    gpu.rectangle('fill', 0, 0, gpu.getWidth(), gpu.getHeight())
 			gpu.setShader()
 		gpu.setCanvas()
@@ -205,21 +244,27 @@ function love.draw()
 		minimapIsDrawed = true
 	end
 
+	gpu.setColor(1.0, 1.0, 1.0, 1.0)
 	gpu.draw(juliaSet)
-	gpu.setColor(1, 1, 1, 0.75)
+	gpu.setColor(1.0, 1.0, 1.0, 0.75)
 	gpu.circle("fill", -5 + screenWidth-0.1*screenHeight, -5 + 0.9*screenHeight, 0.1*screenHeight)
-	gpu.setColor(0, 0, 0)
+	gpu.setColor(0.0, 0.0, 0.0, 1.0)
 	gpu.circle("line", -5 + screenWidth-0.1*screenHeight, -5 + 0.9*screenHeight, 0.1*screenHeight)
+	gpu.setColor(1.0, 1.0, 1.0, 1.0)
 	gpu.draw(minimap, -5 + 0.9*screenWidth - 0.1*screenHeight, -5 + 0.8*screenHeight, 0, 0.2, 0.2)
 
 	local minimapShiftX, minimapShiftY = (0.2*screenWidth)*shift[1]/(2*r), (0.2*screenHeight)*shift[2]/(2*r)
-	gpu.setColor(1, 0, 0)
+	gpu.setColor(1.0, 0.0, 0.0, 1.0)
 	gpu.circle("line", -5 + screenWidth-0.1*screenHeight + minimapShiftX, -5 + 0.9*screenHeight + minimapShiftY, 0.1*screenHeight * zoom)
 	gpu.circle("fill", -5 + screenWidth-0.1*screenHeight + minimapShiftX, -5 + 0.9*screenHeight + minimapShiftY, 2)
 
+	gpu.setColor(0.98, 0.98, 0.98, 0.75)
+	gpu.circle("fill", selector.pos.x, selector.pos.y, selector.boundaryCircleRadius)
+	gpu.setColor(1.0, 1.0, 1.0, 1.0)
+	gpu.draw(mandelbrotSet)
 	selector:draw()
 
-	gpu.setColor(0.1, 0.1, 0.1)
+	gpu.setColor(0.1, 0.1, 0.1, 1.0)
 	gpu.print("FPS: " .. love.timer.getFPS(), 0, 0)
 	gpu.print("Precision: " .. iterations .. ", radius: " .. r, 0, 10)
 	gpu.print("C = " .. const[1] .. ((const[2] > 0) and " + " or " - ") .. math.abs(const[2]) .. "i", 0, 20)
