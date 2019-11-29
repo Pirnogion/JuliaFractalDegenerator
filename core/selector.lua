7e47d5c67e72f1ca18cad1395ad5d1ca32d03520
Pirnogion <glebprositapow@gmail.com>
2019-11-29 17:30:30 +0500

    Random fixes

--- core/selector.lua
+++ core/selector.lua
@@ -1,3 +1,7 @@
+local function map(x, in_min, in_max, out_min, out_max)
+	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+end
+
 local function new(g, px, py, vx, vy, bcr, scr)
 	local g = g
 
@@ -11,40 +15,25 @@ local function new(g, px, py, vx, vy, bcr, scr)
 		boundaryCircleRadius = bcr,
 		selectorCircleRadius = scr,
 
-		style = {
-			outline = {
-				thickness = 3,
-				color = {0.1, 0.1, 0.1}
-			},
-
-			lines = {
-				thickness = 1,
-				color = {0.98, 0.98, 0.98}
-			},
-		},
-
 		draw = function(self)
 			-- cross --
-			g.setColor(self.style.outline.color)
-			g.setLineWidth(self.style.lines.thickness)
+			g.setColor(0.1, 0.1, 0.1)
 			g.line(self.pos.x - self.selectorCircleRadius/2, self.pos.y, self.pos.x + self.selectorCircleRadius/2, self.pos.y)
 			g.line(self.pos.x, self.pos.y - self.selectorCircleRadius/2, self.pos.x, self.pos.y + self.selectorCircleRadius/2)
 
-			-- boundary and selector --
-			g.setColor(self.style.outline.color)
-			g.setLineWidth(self.style.outline.thickness)
+			-- boundary --
+			g.setColor(0.1, 0.1, 0.1)
 			g.circle("line", self.pos.x, self.pos.y, self.boundaryCircleRadius)
-			g.circle("fill", self.pos.x + self.value.x*self.boundaryCircleRadius, self.pos.y + self.value.y*self.boundaryCircleRadius, self.selectorCircleRadius + 2)
 
-			g.setColor(self.style.lines.color)
-			g.setLineWidth(self.style.lines.thickness)
-			g.circle("line", self.pos.x, self.pos.y, self.boundaryCircleRadius)
-			g.circle("fill", self.pos.x + self.value.x*self.boundaryCircleRadius, self.pos.y + self.value.y*self.boundaryCircleRadius, self.selectorCircleRadius)
+			-- selector --
+			g.setColor(0.98, 0.1, 0.1)
+			g.circle("line", self.pos.x + self.value.x*self.boundaryCircleRadius, self.pos.y + self.value.y*self.boundaryCircleRadius, self.selectorCircleRadius + 2)
+			g.circle("fill", self.pos.x + self.value.x*self.boundaryCircleRadius, self.pos.y + self.value.y*self.boundaryCircleRadius, 2)
 
 			-- initial constant value --
 			local str = string.format("C = %.3f %s %.3fi", self.value.x, (self.value.y > 0) and '+' or '-', math.abs(self.value.y))
 
-			g.setColor(self.style.outline.color)
+			g.setColor(0.1, 0.1, 0.1, 0.75)
 			g.print(str, self.pos.x - g.getFont():getWidth(str)/2, self.pos.y + self.boundaryCircleRadius)
 		end,
 
@@ -60,8 +49,8 @@ local function new(g, px, py, vx, vy, bcr, scr)
 			end
 
 			if self.isSelecting then
-				self.value.x = self.value.x + (mx - mouse.prev.x) * 1/2 * dt
-				self.value.y = self.value.y + (my - mouse.prev.y) * 1/2 * dt
+				self.value.x = map(mx, love.graphics.getWidth()-self.boundaryCircleRadius*2 - 5, love.graphics.getWidth() - 5, -1, 1)
+				self.value.y = map(my, 5, self.boundaryCircleRadius*2 + 5, -1, 1)
 
 				if self.value.x^2 + self.value.y^2 > 1 then
 					local len = math.sqrt(self.value.x^2 + self.value.y^2)
